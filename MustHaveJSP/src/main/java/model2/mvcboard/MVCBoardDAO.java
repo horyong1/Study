68d5a78ac6f78b56dc9d6e589716c2436a02f4ef
potato <kmn0848@naver.com>
2022-08-18 17:59:49 +0900

    study

--- MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
+++ MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -1,5 +1,6 @@
 package model2.mvcboard;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -116,4 +117,51 @@ public class MVCBoardDAO extends DBConnPool {
 		}
 		return result;
 	}
+	
+	// 주어진 일련번호에 해당하는 게시물을 DTO에 담아 반환
+	public MVCBoardDTO selectView(String idx) {
+		MVCBoardDTO dto = new MVCBoardDTO();
+		String query = "SELECT * FROM mvcboard WHERE idx=?";
+		
+		try {
+			psmt= con.prepareStatement(query);
+			psmt.setString(1, idx);
+			rs=psmt.executeQuery();
+			
+			if(rs.next()) {
+				dto.setIdx(rs.getString(1));
+				dto.setName(rs.getString(2));
+				dto.setTitle(rs.getString(3));
+				dto.setContent(rs.getString(4));
+				dto.setPostdate(rs.getDate(5));
+				dto.setOfile(rs.getString(6));
+				dto.setSfile(rs.getString(7));
+				dto.setDowncount(rs.getInt(8));
+				dto.setPass(rs.getString(9));
+				dto.setVisitcount(rs.getInt(10));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("게시물 상세보기 중 예외 발생");
+			e.printStackTrace();
+		}
+		return dto;
+	}
+	
+	
+	// 주어진 일련번호에 해당하는 게시물의 조회수를 1증가
+	public void updateVisitCount(String idx) {
+		String query = "UPDATE mvcboard SET "
+					+ " visitcount=visitcount+1"
+					+ " WHERE idx=?";
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, idx);
+			psmt.executeQuery();
+			
+		}catch(Exception e) {
+			System.out.println("게시물 조회수 증가 중 예외 발생");
+			e.printStackTrace();
+		}
+	}				
 }
