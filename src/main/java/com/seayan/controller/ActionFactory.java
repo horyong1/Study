398f0c404070b280c25fde3ea8ab9a63d3de1bef
potato <kmn0848@naver.com>
2022-09-02 11:45:04 +0900

    boardList add

--- src/main/java/com/seayan/controller/ActionFactory.java
+++ src/main/java/com/seayan/controller/ActionFactory.java
@@ -1,6 +1,7 @@
 package com.seayan.controller;
 
 import com.seayan.controller.action.Action;
+import com.seayan.controller.action.BoardListAction;
 
 public class ActionFactory {
 	private static ActionFactory instance = new ActionFactory();
@@ -16,6 +17,11 @@ public class ActionFactory {
 	public Action getAction(String command) {
 		Action action = null;
 		System.out.println("ActionFactory : " + command );
+		
+		if(command.equals("board_list")){
+			action = new BoardListAction();	
+		}
+		
 		return action;
 	}
 }
