28f3684786503aade1707bb094ce9731baab6ff5
potato <107034858+horyong1@users.noreply.github.com>
2023-09-12 18:11:02 +0900

    Add files via upload

---
+++ WebFront/css/Main.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("dfadaadada");
+        for(int i = 0; i<10; i++){
+            
+            for(int j = 0; j<i; j++){
+                System.out.print("@");
+            }      
+            System.out.println("*");
+        }    
+    }
+
+}
