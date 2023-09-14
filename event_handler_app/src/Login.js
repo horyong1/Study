863eafd30140b55317c5eeb791f4135ea0fd0dd1
potato <107034858+horyong1@users.noreply.github.com>
2023-09-14 19:21:11 +0900

    객체 형태 id,pw 값 변경

--- event_handler_app/src/Login.js
+++ event_handler_app/src/Login.js
@@ -1,28 +1,23 @@
 import { useState } from "react";
 
 function Login() {
-  //객체 형태로 id,pw 값 초기화
-  const [inputs, setInputs] = useState({ id: "", pwd: "" });
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
 
-  const { id, pw } = setInputs;
+  //e.target.value e가 이벤트에서 해당 태그 타켓 벨류 값을 가져옴
+  const changeId = function (e) {
+    console.log("id : " + e.target.value);
+    setId(e.target.value);
+  };
 
-  // const [id, setId] = useState("");
-  // const [pw, setPw] = useState("");
+  const changePw = function (e) {
+    console.log("pw : " + e.target.value);
+    // setPw(e.target.value);
 
-  // //e.target.value e가 이벤트에서 해당 태그 타켓 벨류 값을 가져옴
-  // const changeId = function (e) {
-  //   console.log("id : " + e.target.value);
-  //   setId(e.target.value);
-  // };
-
-  // const changePw = function (e) {
-  //   console.log("pw : " + e.target.value);
-  //   // setPw(e.target.value);
-
-  //   //e.target value 를 const { value } 에 넣어 바로 value 값을 사용 가능하게 함
-  //   const { value } = e.target;
-  //   setPw(value);
-  // };
+    //e.target value 를 const { value } 에 넣어 바로 value 값을 사용 가능하게 함
+    const { value } = e.target;
+    setPw(value);
+  };
 
   return (
     <>
