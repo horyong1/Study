f7c9c8c717e70d72cc244cd4a3b4abc7f73b4064
이호룡 <kmn0848@naver.com>
2024-09-09 23:24:13 +0900

    문제 풀이

--- P0909_1/src/Q9/Main.java
+++ P0909_1/src/Q9/Main.java
@@ -2,12 +2,15 @@ package Q9;
 
 public class Main {
 	public static void main(String[] args) {
-		boolean bl = true;
-		for(int i = 2; i <= 100; i++) {
-			for(int j = 2; j<i; j++) {
-				if(i % j != 0) {
-					
+		for(int i = 1; i <= 100; i++) {
+			int cnt = 0;
+			for(int j = 1; j<=i; j++) {
+				if(i % j == 0) {
+					cnt++;
 				}
+			}
+			if(cnt == 2) {
+				System.out.println(i);
 			}
 		}
 		
