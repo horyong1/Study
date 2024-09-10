c31ebbed5d5dc6a352317d6cc94f3d322d466f1c
이호룡 <kmn0848@naver.com>
2024-09-10 23:56:01 +0900

    조깅문제

--- P0910_1/src/Q1.java
+++ P0910_1/src/Q1.java
@@ -10,47 +10,38 @@ public class Q1 {
 		int D = sc.nextInt();
 		int E = sc.nextInt();
 		int F = sc.nextInt();
-		int G = sc.nextInt();
+		int X = sc.nextInt();
+		sc.close();
 		
-		int result1 = 0;
-		int result2 = 0;
-		int sce1 = 0;
-		int sce2 = 0;
-		
-
+		int cycleT = A + C;						// 걷는시간+휴식시간 
+		int fcycleT = X / cycleT;				// 전체 주기 반복 횟수 
+		int remainingTimeT = X % cycleT;		// 전체 주기 후 남는 시간
+		int distanceT = fcycleT * A * B ;		// 주기 반복 횟수와 걷는 거리 계산
 		// Takahashi
-		if(A > G) {
-			result1 = (A - G)* B;
-		}else if( A + C >= G ) {
-			result1 = A * B;
+		if(remainingTimeT <= A) {			
+			distanceT += remainingTimeT * B;	// 남는 시간이 A보다 적으면 남는 시간 * 속도
 		}else {
-			sce1 = G /(A+C);
-			result1 = (sce1+A)* B;
-			int s = G % (A + C);
-			result1 = result1 + (A-s)*B; 
+			distanceT += A * B;					// 남는 시간이 A보다 크면 걷는 시간 * 속도
 		}
 		
-		
+		int cycleA = D + F;
+		int fcycleA = X / cycleA;
+		int remainingTimeA = X % cycleA;
+		int distanceA = fcycleA * D * E;
 		// Aoki
-		if(D > G) {
-			result2 = (D - G)* E;
-		}else if( D + F >= G) {
-			result2 = D * E; 
-		}else {			
-			sce2 = G/(D+F);
-			result2 = (D * E)* sce2;
-			int s = G % (D + F);
-			result2 = result2 + (D-s)*E; 
+		if(remainingTimeA <= D) {
+			distanceA += remainingTimeA * E;
+		}else {
+			distanceA += D * E;	
 		}
 		
 		
-		
-		if(result1 > result2) {
+		if(distanceT > distanceA) {
 			System.out.println("Takahashi");
-		}else if(result1 < result2) {
+		}else if(distanceT < distanceA) {
 			System.out.println("Aoki");
 		}else {
-			System.out.println("draw");
+			System.out.println("Draw");
 		}
 		
 		
