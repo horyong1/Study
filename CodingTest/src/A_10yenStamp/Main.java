6f7e50aa5fd8649921ff4570696f981014a3d9ce
이호룡 <kmn0848@naver.com>
2024-09-14 19:31:18 +0900

    Atcoder 알고리즘 A - 10yen Stamp

---
+++ CodingTest/src/A_10yenStamp/Main.java
@@ -0,0 +1,30 @@
+package A_10yenStamp;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		sc.close();
+		
+		int minus = (Y-X);
+		
+		if(X >= Y) {
+			System.out.println("0");
+			return;
+		}
+		int yen = minus / 10;
+		int yen1 = minus % 10;
+		
+		if(yen1 > 0) {
+			yen += 1;
+		}
+		
+		System.out.println(yen);
+	}
+
+}
