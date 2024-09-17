a76c53f2c363f2a50d0b517b89f52efe079a7ed5
이호룡 <kmn0848@naver.com>
2024-09-17 21:22:13 +0900

    Atcoder 알고리즘 A - Round decimals

---
+++ CodingTest/src/A_Rounddecimals/Main.java
@@ -0,0 +1,24 @@
+package A_Rounddecimals;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double X = sc.nextDouble();
+		sc.close();
+		
+		String str = String.valueOf(X);
+		String[] str2 = str.split("\\.");
+		
+		if(str2[1].charAt(0) >= '5') {
+			int roundValue = Integer.parseInt(str2[0]) + 1;
+			System.out.println(roundValue);
+		}else {
+			System.out.println(str2[0]);
+			
+		}
+				
+//		System.out.println(Math.round(X));
+	}
+}
