e34c3416f53807a6c596eca5d3039699fe246271
이호룡 <kmn0848@naver.com>
2024-09-17 22:17:23 +0900

    Atcoder 알고리즘 A - Seismic magnitude scales

---
+++ CodingTest/src/A_Seismicmagnitudescales/Main.java
@@ -0,0 +1,30 @@
+package A_Seismicmagnitudescales;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		sc.close();
+		
+		int num = A-B;
+		
+		long energy = 1;
+		
+		if(A == B) {
+			System.out.println(1);
+			return;
+		}
+		
+		
+		for(int i = 0; i < num; i++) {
+			energy *= 32;
+		}
+		
+		System.out.println(energy);
+	}
+
+}
