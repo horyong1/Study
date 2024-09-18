d0d7e37d7b7d01b75e2b52aa3528b691d3a8c374
이호룡 <kmn0848@naver.com>
2024-09-18 20:49:15 +0900

    Atcoder 알고리즘 B - Who is missing?

---
+++ CodingTest/src/B_Whoismissing/Main.java
@@ -0,0 +1,30 @@
+package B_Whoismissing;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] cntList = new int[N+1];
+		int inputCnt = N * 4 - 1;
+		
+		for(int i = 0; i < inputCnt; i++) {
+			int num = sc.nextInt();
+			cntList[num]++;
+		}
+		sc.close();
+		
+		int result = 0;
+		for(int i = 1; i <= N; i++) {
+			if(cntList[i] < 4 ) {
+				result = i;
+				break;
+			}
+		}
+		
+		
+		System.out.println(result);
+	}
+}
