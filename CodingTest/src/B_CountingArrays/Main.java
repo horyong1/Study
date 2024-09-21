c21ee6501000c8a47f34ced8b0098db5dc15ee04
이호룡 <kmn0848@naver.com>
2024-09-21 20:19:19 +0900

    AtCoder 알고리즘 B - Counting Arrays

--- CodingTest/src/B_CountingArrays/Main.java
+++ CodingTest/src/B_CountingArrays/Main.java
@@ -1,21 +1,22 @@
 package B_CountingArrays;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	static boolean equal(int[] seq1, int[] seq2) {
-        if (seq1.length != seq2.length) {
-            return false;  
-        }
-        for (int i = 0; i < seq1.length; i++) {
-            if (seq1[i] != seq2[i]) {
-                return false; 
-            }
-        }
-        return true;  
-    }
+//	static boolean equal(int[] seq1, int[] seq2) {
+//        if (seq1.length != seq2.length) {
+//            return false;  
+//        }
+//        for (int i = 0; i < seq1.length; i++) {
+//            if (seq1[i] != seq2[i]) {
+//                return false; 
+//            }
+//        }
+//        return true;  
+//    }
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -35,8 +36,15 @@ public class Main {
 		HashSet<List<Integer>> hashSet = new HashSet<>();
 		
 		for(int[] array : arr) {
-			
+			List<Integer> list = new ArrayList<>();
+			for(int ar : array) {
+				list.add(ar);
+			}
+			hashSet.add(list);
 		}
+		
+		System.out.println(hashSet.size());
+		
 		
 // 답은 맞으나 시간초과로 사용 불가		
 //		boolean check[] = new boolean[N];
