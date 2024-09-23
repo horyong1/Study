cc293fdf8b76367d79e7b813d8e72940a3533444
potato <107034858+horyong1@users.noreply.github.com>
2024-09-23 09:51:24 +0900

    이클립스와 연동

--- P0923_1/src/p1/StudentDto.java
+++ P0923_1/src/p1/StudentDto.java
@@ -1,39 +1,30 @@
-package test;
+package p1;
 
-public class StudentDto{
+public class StudentDto {
 	private String name;
 	private int age;
 	private int score;
 	
-	StudentDto(String name, int age, int score) {
-		this.name = name;
-		this.age = age;
-		this.score = score;
-	}
-
+	
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public int getScore() {
 		return score;
 	}
-
 	public void setScore(int score) {
 		this.score = score;
 	}
+	
 	
 	
 }
