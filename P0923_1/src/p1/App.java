596b756e7ffb7c7e4fca5f2eef3b5d1b3522aef7
potato <107034858+horyong1@users.noreply.github.com>
2024-09-23 12:45:31 +0900

    인스턴트 필드, 생성자, 메서드

--- P0923_1/src/p1/App.java
+++ P0923_1/src/p1/App.java
@@ -2,7 +2,20 @@ package p1;
 
 public class App {
 	public static void main(String[] args) {
-		StudentDto st1 = new StudentDto();
-		StudentDto st2 = new StudentDto();
+		StudentDto st1 = new StudentDto("한조", 30, 50);
+		StudentDto st2 = new StudentDto("트레", 20, 30);
+		StudentDto st3 = new StudentDto();
+		
+		st1.test1();
+		st1.test1();
+		st1.test1();
+
+		st1.test2(3, "안녕");
+		st1.test2(3, "안녕");
+		st1.test2(3, "안녕");
+	
+		int a = st1.test3();
+		
+		int studentCount = 0;
 	}
 }
--- P0923_1/src/p1/StudentDto.java
+++ P0923_1/src/p1/StudentDto.java
@@ -1,30 +1,42 @@
 package p1;
 
 public class StudentDto {
-	private String name;
+	// 속성 - 모든 속성은 private
+	private String name = "";
 	private int age;
 	private int score;
 	
+	// NoArgConstructor
+	public StudentDto() {
+		System.out.println("생성자1 호출됨!!!");
+	}
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
+	// 생성자 - 실행 시점 - 각 인스턴스 생성 직후
+	// AllArgConstructor
+	public StudentDto(String name, int age, int score) {
+		// 로직 구현 가능
+		System.out.println("생성자2 호출됨!!!");
 		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
 		this.age = age;
-	}
-	public int getScore() {
-		return score;
-	}
-	public void setScore(int score) {
 		this.score = score;
 	}
 	
 	
+	// 기능 - 언젠가 누군가가 호출해야 실행됨. 다중으로 호출 가능
+	public void test1() {
+		// 로직 정의... 안쪽에서 this가 안쓰이면. 응집도에 문제가 있을 수 있다.
+		System.out.println("test1 로직 실행됨....: " + name);
+	}
+	
+	
+	public void test2(int a, String b) {
+		System.out.println("test2 로직 실행됨....: " + a + b);
+	}
+	
+	public int test3() {
+		System.out.println("test3 로직 실행됨......: " + score);
+		int result = score + 3;
+		return result;
+	}
 	
 }
