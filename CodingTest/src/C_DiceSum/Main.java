ceeec4535811acc236b48ebf3776b0132db1e089
potato <107034858+horyong1@users.noreply.github.com>
2024-09-23 17:34:27 +0900

    AtCoder 알고리즘 C - Dice Sum

---
+++ CodingTest/src/C_DiceSum/Main.java
@@ -0,0 +1,33 @@
+package C_DiceSum;
+
+import java.util.Scanner;
+
+public class Main {
+	private static final int MOD = 998244353;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int K = sc.nextInt();
+		
+		long[][] dp = new long[N+1][K+1];
+		dp[0][0] = 1;
+		
+		for(int i = 1; i <= N; i++ ) {
+			for(int j = 0; j <= K; j++) {
+				for(int x = 1; x <= M; x++) {
+					if(j - x >= 0) {
+						dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % MOD;
+					}
+				}
+			}
+		}
+		long result = 0;
+		for(int j = 0; j <= K; j++) {
+			result = (result + dp[N][j]) % MOD;
+		}
+		
+		System.out.println(result);
+	}
+}
+
