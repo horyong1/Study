6df8a31e107b29437f540c0df87ba8b24b7e75f4
potato <107034858+horyong1@users.noreply.github.com>
2024-09-26 13:40:53 +0900

    학생관리프로그램 인터페이스로 구현하기 연습 생성

---
+++ StudentManagerV3/src/com/student/dto/StudentDto.java
@@ -0,0 +1,45 @@
+package com.student.dto;
+
+public class StudentDto {
+	private String name;
+	private int age;
+	private int score;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
