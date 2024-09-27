9e8d8ac32c12e22bdbda23ebc37e6bd49b39a145
potato <107034858+horyong1@users.noreply.github.com>
2024-09-27 10:18:32 +0900

    removeCount,removeStudentCountPrint 메소드 생성 코드분리

--- StudentManagerV3/src/com/student/component/RemoveStudentServiceImpl.java
+++ StudentManagerV3/src/com/student/component/RemoveStudentServiceImpl.java
@@ -14,8 +14,20 @@ public class RemoveStudentServiceImpl implements Service{
 	@Override
 	public void process() {
 		IoUtil.print("=====[학생 삭제]=====");
-		int result = repository.removeByName(IoUtil.input("삭제할 학생 이름 >>>> "));
+		int result = removeCount();
+		removeStudentCountPrint(result);
+	}
+	
+	/**
+	 * 학생 정보 삭제 개수
+	 * */
+	private int removeCount() {
+		return repository.removeByName(IoUtil.input("삭제할 학생 이름 >>>> "));
+	}
+	/**
+	 * 학생 정보 삭제 출력
+	 * */
+	private void removeStudentCountPrint(int result) {
 		IoUtil.print("학생 " + result + "명이 삭제 되었습니다.");
 	}
-
 }
