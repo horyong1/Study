d40adba1db5598496867f27b2c419628dfb46b19
potato <107034858+horyong1@users.noreply.github.com>
2024-09-30 08:52:06 +0900

    프로그래머스 레벨0 최빈값 구하기

--- CodingTest/src/programmers/basic/level0/최빈값_구하기/Main.java
+++ CodingTest/src/programmers/basic/level0/최빈값_구하기/Main.java
@@ -1,6 +1,7 @@
 package programmers.basic.level0.최빈값_구하기;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -10,36 +11,57 @@ public class Main {
 	}
 
 }
+
 class Solution {
 	private int[] index;
     public int solution(int[] array) {
+    	// 첫번째 방법
+//    	int result = 0;
+//    	Arrays.sort(array);
+//    	int cnt = array[array.length-1];
+//        index = new int[cnt+1];
+//        
+//        if(array.length == 1) {
+//        	return array[0];
+//        }
+//        
+//        for(int i = 0; i <array.length; i++) {
+//        	index[array[i]]++;
+//        }
+//        
+//        int max = 0;
+//        for(int i = 0; i < index.length; i++) {
+//        	int value = index[i];
+//        	max = Math.max(max, value);
+//        }
+//        int count = 0;
+//        for(int i = 0; i < index.length; i++) {
+//        	if(index[i] == max) {
+//        		result = i;
+//        		count++;
+//        	}
+//        }
+//        
+//        
+//        return count > 1 ? -1 : result;
+    	
     	int result = 0;
-    	Arrays.sort(array);
-    	int cnt = array[array.length-1];
-        index = new int[cnt+1];
-        
-        if(array.length == 1) {
-        	return array[0];
-        }
-        
-        for(int i = 0; i <array.length; i++) {
-        	index[array[i]]++;
-        }
-        
-        int max = 0;
-        for(int i = 0; i < index.length; i++) {
-        	int value = index[i];
-        	max = Math.max(max, value);
-        }
-        int count = 0;
-        for(int i = 0; i < index.length; i++) {
-        	if(index[i] == max) {
-        		result = i;
-        		count++;
-        	}
-        }
-        
-        
-        return count > 1 ? -1 : result;
+    	int maxCount = 0;
+    	
+    	Map<Integer, Integer> map = new HashMap<>();
+    	
+    	for(int number : array) {
+    		int count = map.getOrDefault(number, 0) + 1;
+    		
+    		if(count > maxCount) {
+    			maxCount = count;
+    			result = number;
+    			
+    		}else if (count == maxCount){
+    			result = -1;
+    		}
+    		map.put(number, count);
+    	}
+    	return result;
     }
 }
