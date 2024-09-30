f71acc61c995b02bfbf4598c85159650c6df917c
potato <107034858+horyong1@users.noreply.github.com>
2024-09-30 14:05:37 +0900

    프로그래머스 레벨0 피자나눠먹기3

---
+++ java/src/level0/pizza3/Main.java
@@ -0,0 +1,13 @@
+package level0.pizza3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int solution(int slice, int n) {
+        return  (n + (slice - 1)) / slice;
+    }
+}
